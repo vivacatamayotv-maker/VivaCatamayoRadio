f336b40fd72cf1f83dc57c3eec578a15347ae2db
vivacatamayotv-maker <vivacatamayotv@gmail.com>
2026-09-06 18:12:38 -0500

    v1.2: agregar acceso oficial a Facebook

--- app/src/main/java/com/vivacatamayo/radio/RadioConfig.kt
+++ app/src/main/java/com/vivacatamayo/radio/RadioConfig.kt
@@ -7,5 +7,6 @@ object RadioConfig {
     const val STREAM_URL = "https://sonic.streamseguro.com/8116/stream"
     const val WEBSITE_URL = "https://vivacatamayo.com/"
     const val RADIO_PAGE_URL = "https://vivacatamayo.com/radio-web-catamayo-2026/"
+    const val FACEBOOK_URL = "https://www.facebook.com/VivaCatamayoTV"
     const val WHATSAPP_NUMBER = "593987660490"
 }
