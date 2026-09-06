ce5c3776048e17ef0f3c9342155afeefa584cb4a
vivacatamayotv-maker <vivacatamayotv@gmail.com>
2026-09-06 18:12:45 -0500

    v1.2: actualizar versión Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vivacatamayo.radio"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     buildTypes {
